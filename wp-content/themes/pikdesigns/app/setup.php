aa6881f74ad5cef9e752b0d10f4977bbc4cc0ca1
janak shrestha <janak.s@octomedia.com.au>
2024-08-29 16:26:33 +0545

    under construction page styling

--- wp-content/themes/pikdesigns/app/setup.php
+++ wp-content/themes/pikdesigns/app/setup.php
@@ -97,6 +97,28 @@ add_action('after_setup_theme', function () {
      * @link https://developer.wordpress.org/reference/functions/add_theme_support/#customize-selective-refresh-widgets
      */
     add_theme_support('customize-selective-refresh-widgets');
+
+    // Allow SVG
+    add_filter('wp_check_filetype_and_ext', function ($data, $file, $filename, $mimes) {
+
+        global $wp_version;
+        if ($wp_version !== '4.7.1') {
+            return $data;
+        }
+
+        $filetype = wp_check_filetype($filename, $mimes);
+
+        return [
+            'ext'             => $filetype['ext'],
+            'type'            => $filetype['type'],
+            'proper_filename' => $data['proper_filename']
+        ];
+    }, 10, 4);
+
+    add_filter('upload_mimes', function ($mimes) {
+        $mimes['svg'] = 'image/svg+xml';
+        return $mimes;
+    });
 }, 20);
 
 /**
