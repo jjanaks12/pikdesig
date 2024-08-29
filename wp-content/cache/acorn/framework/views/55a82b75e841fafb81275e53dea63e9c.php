8d3b28aa9faa5402fd7b5a529e078439b7bcf20c
janak shrestha <janak.s@octomedia.com.au>
2024-08-29 11:56:02 +0545

    pikdesign initialized

---
+++ wp-content/cache/acorn/framework/views/55a82b75e841fafb81275e53dea63e9c.php
@@ -0,0 +1,4 @@
+<div class="page-header">
+  <h1><?php echo $title; ?></h1>
+</div>
+<?php /**PATH /Users/janakshrestha/Documents/2024/08 August/pikdesig/wp-content/themes/pikdesigns/resources/views/partials/page-header.blade.php ENDPATH**/ ?>
---
+++ wp-content/cache/acorn/framework/views/bcca3e69a4af0f7a7792edeef01f622a.php
@@ -0,0 +1,4 @@
+<footer class="content-info">
+  <?php (dynamic_sidebar('sidebar-footer')); ?>
+</footer>
+<?php /**PATH /Users/janakshrestha/Documents/2024/08 August/pikdesig/wp-content/themes/pikdesigns/resources/views/sections/footer.blade.php ENDPATH**/ ?>
