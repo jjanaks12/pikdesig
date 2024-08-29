ccf6ee22bc5bc68840164e096bae6827cb546840
janak shrestha <janak.s@octomedia.com.au>
2024-08-29 12:31:23 +0545

    theme updated

---
+++ wp-content/cache/acorn/framework/views/2529324ab5ff6fdbd8238b3bdb97c283.php
@@ -0,0 +1,3 @@
+
+
+<h1>Under construction</h1><?php /**PATH /Users/janakshrestha/Documents/2024/08 August/pikdesig/wp-content/themes/pikdesigns/resources/views/home.blade.php ENDPATH**/ ?>
--- wp-content/themes/pikdesigns/bud.config.js
+++ wp-content/themes/pikdesigns/bud.config.js
@@ -34,7 +34,7 @@ export default async (app) => {
    */
   app
     .setUrl('http://localhost:3000')
-    .setProxyUrl('http://example.test')
+    .setProxyUrl('https://pikdesig.test/')
     .watch(['resources/views', 'app']);
 
   /**
